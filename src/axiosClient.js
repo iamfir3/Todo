61ab6ecefa234231f0d6d97701d17d1698474faf
iamfir3 <88335051+iamfir3@users.noreply.github.com>
2023-03-10 14:18:00 +0700

    update

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClients = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://todoback-production-8ba8.up.railway.app',
 });
 
 export default axiosClients;
