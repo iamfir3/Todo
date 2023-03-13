c4b73b306e628f7b66ca3f22a5a2a87ad0888efa
iamfir3 <88335051+iamfir3@users.noreply.github.com>
2023-03-13 08:03:19 +0700

    update

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -180,6 +180,7 @@ const TodoItem = ({
                   }
                 };
                 updateTodo();
+                inputRef.current.value='';
               }}
             ></BsFillCheckCircleFill>
           )}
